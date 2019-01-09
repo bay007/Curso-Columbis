3b353b036311cb48dd2b2267501d541952fddf9a
bay007 <tnt.galicia@gmail.com>
2019-01-09 01:31:58 -0600

    agregar el ejercicio #2

---
+++ ejercicio_2.py
@@ -0,0 +1,10 @@
+"""
+- Definir una función que calcule la longitud de una lista o una 
+cadena dada. (Es cierto que python tiene la función len() 
+incorporada, pero escribirla por nosotros mismos resulta un muy 
+buen ejercicio.
+
+PD:Puedes usar la funcion input para mejorar tu programa, 
+También se sugiere usar el uso del método en cadenas stip()
+
+"""
