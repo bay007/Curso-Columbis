d29422705846caf0716660761e7f843b22cc987b
bay007 <tnt.galicia@gmail.com>
2019-01-09 01:33:30 -0600

    Agregar ejercicio 3

---
+++ ejercicio_3.py
@@ -0,0 +1,8 @@
+"""
+Escribir una función que tome un carácter y devuelva True si es una vocal,
+de lo contrario devuelve False
+
+
+PD: Es importante consultar una tabla ASCII, 
+seguramente la función ord() y char() serán de ayuda.
+"""
