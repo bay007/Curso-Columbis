b9d5c9190f1ba6c3d2c493a8aaef2aad4a4997bd
nancycm <cm_nancy@hotmail.com>
2019-01-09 10:22:32 -0600

    Se resuelve el conteo de elementos de una lista

--- ejercicio_2.py
+++ ejercicio_2.py
@@ -5,6 +5,15 @@ incorporada, pero escribirla por nosotros mismos resulta un muy
 buen ejercicio.
 
 PD:Puedes usar la funcion input para mejorar tu programa, 
-También se sugiere usar el uso del método en cadenas stip()
+También se sugiere usar el uso del método en cadenas strip()
 
 """
+
+numeros_pares = [2,4,6,8,10,12,14]
+elementos = 0
+
+for numero_par in numeros_pares:
+    elementos = elementos + 1
+
+print (f"La lista tiene {elementos} números")
+
