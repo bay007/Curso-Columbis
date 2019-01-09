6fbcd54f306e4312e316f85d3634c25c13d7eb2a
bay007 <tnt.galicia@gmail.com>
2019-01-09 01:45:37 -0600

    Ejercicio_4

---
+++ ejercicio_4.py
@@ -0,0 +1,8 @@
+"""
+Escribir una función con nombre cadena_mas_amplia() 
+que tome como parametro una lista de palabras o una frase
+y devuelva la mas amplia. 
+
+
+PD: El Método de cadena split() podrá ayudar.
+"""
