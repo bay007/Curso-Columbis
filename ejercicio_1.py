88f17f97a964b559fb33868c6fe96007b1c86496
bay007 <tnt.galicia@gmail.com>
2019-01-09 01:28:10 -0600

    Se agrega el ejercicio 1

---
+++ ejercicio_1.py
@@ -0,0 +1,10 @@
+"""
+Escribe un programa que pida dos palabras y diga si riman o no.
+Si coinciden las tres últimas letras tiene que decir que riman.
+Si coinciden sólo las dos últimas tiene que decir que riman un
+poco y si no, que no riman.
+
+PD: investiga el uso de la función input(), con ella podrás mejorar 
+tu programa
+
+"""
