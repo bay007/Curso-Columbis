0f4eb23912651b6f7045b693e42601bbfe4ba167
Ernestozc <zavalacortes@hotmail.com>
2019-01-16 08:21:46 -0600

    ejercicio 8

--- ejercicio_8.py
+++ ejercicio_8.py
@@ -6,3 +6,20 @@ PD: Para éste ejemplo  hay que hacer uso del módulo random
 https://docs.python.org/3.7/library/random.html
 
 """
+from random import choice
+
+def dado():
+    numeros = [1,2,3,4,5,6]
+    valor= choice(numeros)
+    
+    return valor
+
+print(dado())
+
+#List Compre
+"""
+k=[numero for numero in range(8) if numero>3]
+print(k)
+"""
+
+
