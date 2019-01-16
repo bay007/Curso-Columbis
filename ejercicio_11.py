f9feacf08b91810e79a95f3719904de30238016d
MINA250585 <mina_palacios@hotmail.com>
2019-01-16 10:02:15 -0600

    se hicieorn cambios a ejercicio 11

--- ejercicio_11.py
+++ ejercicio_11.py
@@ -11,3 +11,29 @@ PD: Se sugiere hacer uso del paquete Faker.
 En su linea de comandos ejecute
 pip install Faker
 """
+#pprint pprint o import jason  -- jason.dumps()
+
+
+from faker import Faker
+from pprint import pprint 
+import json 
+
+def obten_datos_persona(num_datos):
+    num = 0
+    fake = Faker('') 
+    datos = ""
+    
+    for dato in range(int(num_datos)):
+        num = num + 1
+        lista_datos = {"id": num,
+                        "Nombre": fake.name(),
+                        "Direccion_pos":fake.address(),
+                        "Direccion_fiscal":fake.address()}
+        print(lista_datos)
+        datos = datos + str(lista_datos)
+    return datos
+ 
+
+resultado =  obten_datos_persona(input("¿Cuantos datos quieres obtener?"))
+
+print(json.dumps(resultado, indent = 3))
