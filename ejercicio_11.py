ec745f46f75cae7403702d022821f008de9c5aa4
hrodrich <hrodrich.26@gmail.com>
2019-01-16 09:45:48 -0600

    Se completa el ejecicio 11

--- ejercicio_11.py
+++ ejercicio_11.py
@@ -11,3 +11,31 @@ PD: Se sugiere hacer uso del paquete Faker.
 En su linea de comandos ejecute
 pip install Faker
 """
+
+from faker import Faker
+from pprint import pprint
+import json
+
+fake = Faker('es_MX')
+fake2 = Faker('es_MX')
+
+def pf(numeros_pf = 1):
+    personas_json = []
+    for contador in range(numeros_pf):
+        name = fake.name()
+        address = fake.address()
+        address2 = fake.address()
+        '''personas = {'nombre': name,
+                    'direccion_fisica': address,
+                    'direccion_postal': address2}'''
+        personas = {'nombre': name,
+                    'direccion_fisica': address,
+                    'direccion_postal': address2}
+        personas_json.append(personas)
+        #print(personas)
+    print(json.dumps(personas_json, indent=4))
+    print(len(personas_json))
+
+pf()
+
+
