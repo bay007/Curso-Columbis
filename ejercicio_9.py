9082da82707e4b57c1be00347f4ec4eac0aec13b
MINA250585 <mina_palacios@hotmail.com>
2019-01-22 08:14:49 -0600

    cambio ejercicio 9

--- ejercicio_9.py
+++ ejercicio_9.py
@@ -4,15 +4,46 @@ que refleje la frecuencia de ocurrencia de cada letra que aparezca en la frase. 
 debe importar, el conteo debe hacerse.
 """
 
+import string
 
-def cuenta_caracteres(frase):
-    vocales = {"a":0,"e":0,"i":0, "o":0,"u":0}
-    cuenta = 0
-    for letra in frase:
-        print(letra)
+#string.ascii_lowercase
+letras_minus = string.ascii_lowercase + " "
+alfabeto_minus = { x:0 for x in letras_minus }
+#print(alfabeto_minus)
+
+#string.ascii_uppercase
+letras_mayus  = string.ascii_uppercase + " " 
+alfabeto_mayus = { x:0 for x in letras_mayus}
+#print(alfabeto_mayus)
+
+def cuenta_caracteres(frase):  
+    print(frase)    
+    valor_min = 0
+    valor_may = 0
+    for letra_min in frase:
+        if letra_min in alfabeto_minus: 
+                valor_min = alfabeto_minus.get(letra_min)
+                valor_min = valor_min + 1
+                alfabeto_minus[letra_min] = valor_min
+    for letra_may in frase:            
+        if letra_may in alfabeto_mayus: 
+                valor_may = alfabeto_mayus.get(letra_may)
+                valor_may = valor_may + 1
+                alfabeto_mayus[letra_may] = valor_may
+ 
+    return 
+
+
 
     
-    
+palabra_frase = input("Escribe una palabra o frase:")
+cuenta_caracteres(palabra_frase)
 
-    
-cuenta_caracteres(input("Escribe una palabra o frase:"))
+print(f"Minusculas:{alfabeto_minus}")
+print(f"Mayusculas:{alfabeto_mayus}")
+
+
+
+#print(letras)
+#letters= dict(alphabet)
+#print(letters)
