e70968e27ef14dcd9d4ca1a414fb6e6eaacadcdc
bay007 <tnt.galicia@gmail.com>
2019-01-28 01:34:11 -0600

    CRUD FLASK

--- flask/app.py
+++ flask/app.py
@@ -1,74 +1,78 @@
-import json
-import os
-
-from flask import (Flask, make_response, redirect, render_template, request,
-                   request_started, session, url_for, jsonify, abort)
-from werkzeug.routing import BuildError
-from functools import wraps
-
+from flask import Flask, request, jsonify, redirect, Response
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.exceptions import NotFound
 
 app = Flask(__name__)
-app.secret_key = os.urandom(61)
-app.session_cookie_name = "reyes"
-app.apis = 2
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///.\\foo.db'
 
 
-def login_required(function):
-
-    @wraps(function)
-    def wrapper(*args, **kwargs):
-        if "username" not in session:
-            abort(401)
-        return function(*args, **kwargs)
-    return wrapper
+db = SQLAlchemy(app)
 
 
-@app.before_request
-def before():
-    pass
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(80), unique=True, nullable=False,)
+
+    def __repr__(self):
+        return self.username
 
 
-@app.errorhandler(BuildError)
-def builderror(error):
-    return "BuildError"
+@app.route("/users", methods=["POST"])
+def users_create():
+    request_data = request.get_json()
+    user = User(**request_data)
+    db.session.add(user)
+    db.session.commit()
+    return Response(response="ok", status=201)
 
 
-@app.route("/users")
-@login_required
-def users():
-    with open("files.json", encoding="utf-8", mode="r") as fp:
-        users = json.load(fp)
-        return jsonify(users)
-
-
-@app.errorhandler(404)
-def _404(error):
-    r = make_response(f"No encontrado {error}")
-    r.headers["X-remain-request"] = 87
-    return redirect(url_for("index"))
-
-
-@app.route("/", methods=["GET"])
-@login_required
-def index():
-    if session.get("username", False):
-        return render_template("index.html", username=session['username'])
+@app.route("/users", methods=["GET"])
+def users_retrieve():
+    if len(request.args) > 0:
+        users = User.query.filter_by(**request.args)
     else:
-        return "can you login?"
+        users = User.query.all()
+
+    users_response = [dict(id=user.id, username=user.username,
+                           email=user.email) for user in users]
+    return jsonify(users_response), 200
 
 
-@app.route("/login/<username>/", methods=["GET"])
-def login(username):
-    session["username"] = username
-    return f'logueado {session["username"] } '
+@app.route("/users/<id>", methods=["GET"])
+def user_get(id):
+    user = User.query.get(id)
+    if user is None:
+        return Response("usuario no hallado", status=404)
+    return jsonify(dict(id=user.id, username=user.username, email=user.email)), 200
 
 
-@app.route("/logout", methods=["GET"])
-def logout():
-    if "username" in session:
-        del session["username"]
-    return "deslogueado"
+@app.route("/users/<id>", methods=["PUT"])
+def users_update(id):
+    user = User.query.get(id)
+    incoming_user = request.get_json()
+
+    if user is None or incoming_user is None:
+        return Response(response="No hallado", status=404)
+
+    user.username = incoming_user.get("username")
+    user.email = incoming_user.get("email")
+    db.session.commit()
+
+    return "ok", 200
+
+
+@app.route("/users/<id>", methods=["DELETE"])
+def users_delete(id):
+    user = User.query.get(id)
+    if user is None:
+        return NotFound.description, 404
+
+    db.session.delete(user)
+    db.session.commit()
+    return Response(status=204)
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host="127.0.0.1", port=3200)
+    # db.create_all()
+    app.run(port=8085, debug=True)
