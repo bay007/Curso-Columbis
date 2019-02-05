9bd98bd43a8b7eb977ecfb0d8ce3045c60a64ce4
bay007 <tnt.galicia@gmail.com>
2019-02-05 07:11:22 -0600

    Api Básica
    *Declarar una nueva vista para que maneje los metodos
    
    *Declarar nuevas URLS con parametros
    
    *Eliminar la proteccion CSFR

--- encuesta/votacion/urls.py
+++ encuesta/votacion/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from .views import VotacionView_claquiernombre
+from .views import VotacionView_claquiernombre, QuestionsView
 
 
 urlpatterns = [
-    path("", VotacionView_claquiernombre.as_view())
+    path("", VotacionView_claquiernombre.as_view()),
+    path("questions/", QuestionsView.as_view()),
+    path("questions/<uuid:uuid>/", QuestionsView.as_view())
 ]
--- encuesta/votacion/views.py
+++ encuesta/votacion/views.py
@@ -1,6 +1,11 @@
+import json
+
+from django.http import HttpResponse
 from django.shortcuts import render
 from django.views import View
-from django.http import HttpResponse
+
+from .models import Question
+
 # Create your views here.
 
 
@@ -15,6 +20,38 @@ class VotacionView_claquiernombre(View):
 
     def post(self, request):
         pass
+
+    def put(self, request):
+        pass
+
+    def delete(self, request):
+        pass
+
+
+class QuestionsView(View):
+
+    def get(self, request, uuid=None):
+        # Aqui se puede poner la logica de negocio
+        # accesos a bases de datos
+        # validacion de los mismos
+        # escritura de bases de datos.
+        # Etc, Etc , Etc
+        if uuid is None:
+            questions = Question.objects.all()
+            respuesta = []
+            for q in questions:
+                respuesta.append({"uuid": str(
+                    q.uuid), "question_text": q.question_text, "publish_date": str(q.publish_date)})
+        else:
+            r = Question.objects.get(uuid=uuid)
+            respuesta = {"uuid": str(r.uuid), "question_text": r.question_text,
+                         "publish_date": str(r.publish_date)}
+
+        return HttpResponse(json.dumps(respuesta, ensure_ascii=False), content_type="application/json")
+
+    def post(self, request, uuid=None):
+        print(request.POST)
+        return HttpResponse(f"Hola{uuid}")
 
     def put(self, request):
         pass
