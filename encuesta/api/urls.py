4766902ed707def200e131b02ccef7c4f0f4b41c
bay007 <tnt.galicia@gmail.com>
2019-02-08 01:37:32 -0600

    Documentación de la API
    Se sigue la documentacion oficial.
    
    https://marcgibbons.com/django-rest-swagger/

--- encuesta/api/urls.py
+++ encuesta/api/urls.py
@@ -1,10 +1,16 @@
+
 from django.urls import path
 
+from rest_framework_swagger.views import get_swagger_view
 
-from .views import QuestionList, QuestionDetail, ChoiceList
+from .views import ChoiceList, QuestionDetail, QuestionList
+
+schema_view = get_swagger_view(title='API Votaciones')
+
 
 # Se definen los recursos a los que se hara referencia en las URLs
 urlpatterns = [
+    path('docs/', schema_view),
     path('questions/', QuestionList.as_view()),
     path('questions/<uuid:pk>/', QuestionDetail.as_view()),
     path('choices/', ChoiceList.as_view())
